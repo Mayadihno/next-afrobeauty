e6921fdd1ea53a406436c4dee4ff32ebc0bbd8c2
Maya Tunde Olawale <mayadihno@gmail.com>
2024-06-12 11:31:58 +0100

    navbar setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <h2 className=" text-red-500 flex justify-center items-center mt-16">
-      hello there
+      i want to do the navbar here
     </h2>
   );
 }
