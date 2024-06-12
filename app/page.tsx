9179ec697cf9dc8edcb13a685d3fe14741746951
Maya Tunde Olawale <mayadihno@gmail.com>
2024-06-12 12:00:04 +0100

    font family set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h2 className=" text-red-500 flex justify-center items-center mt-16">
-      hello there
-    </h2>
+    <>
+      <h2 className=" text-red-500 flex justify-center items-center mt-16 font-unkempt">
+        hello there
+      </h2>
+      <h2 className=" text-red-500 flex justify-center items-center mt-16 font-prociono">
+        hello there
+      </h2>
+      <h2 className=" text-red-500 flex justify-center items-center mt-16 font-abril">
+        hello there
+      </h2>
+      <h2 className=" text-red-500 flex justify-center items-center mt-16 font-ebgaramond">
+        hello there
+      </h2>
+      <h2 className=" text-red-500 flex justify-center items-center mt-16 font-urbanist">
+        hello there
+      </h2>
+    </>
   );
 }
